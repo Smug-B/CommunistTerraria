05b969fda4df7795d2cfcfaff6bcdd110a4a4b52
Chik3r <ikervaraalarcon@gmail.com>
2021-05-19 22:53:59 -0400

    Finish the manifesto ui

--- Mod/CommunistTerraria_OurManifesto.cs
+++ Mod/CommunistTerraria_OurManifesto.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 using CommunistTerraria.UI;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.UI;
 
@@ -13,20 +14,31 @@ namespace CommunistTerraria
 
         private void LoadManifesto()
         {
+            if (Main.dedServ) return;
+            
             byte[] manifestoBytes = GetFileBytes("manifesto.txt");
             _manifesto = Encoding.UTF8.GetString(manifestoBytes);
 
             _manifestoInterface = new UserInterface();
         }
 
-        public void ToggleManifestoUI()
+        public void ToggleManifestoUI(bool? forceState = null)
         {
 #if DEBUG
             Main.NewText($"Toggling manifesto ui, old state is: {_manifestoInterface.CurrentState}");
 #endif
+            switch (forceState)
+            {
+                case true:
+                    _manifestoInterface.SetState(new ManifestoUI(_manifesto));
+                    return;
+                case false:
+                    _manifestoInterface.SetState(null);
+                    return;
+            }
             
             if (_manifestoInterface.CurrentState == null)
-                _manifestoInterface.SetState(new ManifestoUI());
+                _manifestoInterface.SetState(new ManifestoUI(_manifesto));
             else
                 _manifestoInterface.SetState(null);
         }
@@ -45,6 +57,11 @@ namespace CommunistTerraria
                     return true;
                 },
                 InterfaceScaleType.UI));
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            _manifestoInterface?.Update(gameTime);
         }
     }
 }
--- UI/ManifestoUI.cs
+++ UI/ManifestoUI.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
 using Terraria.GameContent.UI.Elements;
+using Terraria.GameInput;
+using Terraria.ModLoader;
 using Terraria.UI;
 
 namespace CommunistTerraria.UI
@@ -9,6 +14,10 @@ namespace CommunistTerraria.UI
     {
         private readonly string _manifesto;
         private UIPanel _mainPanel;
+        private UIText _mainText;
+        private UIText _currentPageText;
+        private int _currentPage;
+        private List<IEnumerable<string>> _pages;
 
         public ManifestoUI(string manifesto) => _manifesto = manifesto;
 
@@ -23,15 +32,83 @@ namespace CommunistTerraria.UI
             };
             Append(_mainPanel);
 
-            IEnumerable<IEnumerable<string>> pages = SplitPages(_manifesto);
+            _pages = SplitPages(_manifesto).ToList();
 
-            UIText simpleText = new UIText(string.Join("\n", pages.First()))
+            _mainText = new UIText(string.Join("\n", _pages.First()))
             {
                 HAlign = 0f,
                 VAlign = 0f,
                 Height = new StyleDimension(_mainPanel.Height.Pixels - 100, 0),
             };
-            _mainPanel.Append(simpleText);
+            _mainPanel.Append(_mainText);
+
+
+
+            UIPanel pagePanel = new UIPanel
+            {
+                Top = new StyleDimension(_mainPanel.Height.Pixels - 75, 0),
+                HAlign = .5f,
+                Height = new StyleDimension(50, 0),
+                Width = new StyleDimension(200, 0)
+            };
+            _mainPanel.Append(pagePanel);
+
+            Texture2D flippedPlayTexture = ModContent.GetInstance<CommunistTerraria>().GetTexture("Textures/FlippedButtonPlay");
+            UIImageButton backButton = new UIImageButton(flippedPlayTexture)
+            {
+                Left = new StyleDimension(0, 0),
+                VAlign = 0.5f,
+                Width = new StyleDimension(25, 0f)
+            };
+            backButton.OnClick += BackClicked;
+            pagePanel.Append(backButton);
+
+            Texture2D playTexture = ModContent.GetTexture("Terraria/UI/ButtonPlay");
+            UIImageButton nextButton = new UIImageButton(playTexture)
+            {
+                Left = new StyleDimension(150, 0),
+                VAlign = 0.5f,
+                Width = new StyleDimension(25, 0f)
+            };
+            nextButton.OnClick += NextClicked;
+            pagePanel.Append(nextButton);
+
+            _currentPageText = new UIText("001")
+            {
+                HAlign = 0.5f,
+                VAlign = 0.5f
+            };
+            pagePanel.Append(_currentPageText);
+        }
+
+        private void NextClicked(UIMouseEvent evt, UIElement listeningelement)
+        {
+            if (++_currentPage >= _pages.Count)
+            {
+                _currentPage = _pages.Count;
+                return;
+            }
+
+            UpdateText(_currentPage);
+        }
+
+        private void BackClicked(UIMouseEvent evt, UIElement listeningelement)
+        {
+            if (--_currentPage < 0)
+            {
+                _currentPage = 0;
+                return;
+            }
+
+            UpdateText(_currentPage);
+        }
+
+        private void UpdateText(int pageNum)
+        {
+            _currentPageText.SetText((pageNum + 1).ToString("000"));
+
+            string mainText = string.Join("\n", _pages[pageNum]);
+            _mainText.SetText(mainText);
         }
 
         private static IEnumerable<IEnumerable<string>> SplitPages(string manifesto, int lineCount = 20)
@@ -39,7 +116,18 @@ namespace CommunistTerraria.UI
             List<string> lines = manifesto.Split('\n').ToList();
             for (int i = 0; i < lines.Count; i += lineCount)
             {
-                yield return lines.GetRange(i, lineCount);
+                int leftLines = lines.Count - i;
+                yield return lines.GetRange(i, lineCount > leftLines ? leftLines : lineCount);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (_mainPanel.ContainsPoint(Main.MouseScreen) && !PlayerInput.IgnoreMouseInterface)
+            {
+                Main.LocalPlayer.mouseInterface = true;
             }
         }
     }
